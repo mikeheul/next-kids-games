3f3b420bbcb61efc2f1383ed6d8b4718b7fe55f9
Micka <mickael.murmann@gmail.com>
2024-06-28 11:16:16 +0200

    Delete title on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="bg-slate-800">
-      <h1 className="text-4xl mb-6">Jeux pour Enfants</h1>
     </div>
   );
 }
